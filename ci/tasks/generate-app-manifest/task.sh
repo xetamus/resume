d7ce302d7ce1d1f667794faac11fba580609157b
Shishir Amin <samin@pivotal.io>
2018-03-29 02:03:25 -0400

    add concourse pipeline

---
+++ ci/tasks/generate-app-manifest/task.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -eu
+
+touch resume/Staticfile
+
+cat <<EOF > app-manifest/manifest.yml
+---
+applications:
+  - name: $APP_NAME
+    routes:
+      - route: $APP_ROUTE
+    memory: $APP_MEMORY
+    instances: $APP_INSTANCES
+    buildpack: static
+    root: resume
+EOF
